bee2a58f8a047a7fb27cb954df4cea56f047b43e
AngelAlfGar <71830683+AngelAlfGar@users.noreply.github.com>
2021-12-12 00:53:23 -0600

    Add files via upload

---
+++ PYTHON/C8/Entrada_de_datos.py
@@ -0,0 +1,9 @@
+palabra = input("Introduce una palabra: ")
+num_int = int(input("Introduce un numero entero: "))
+num_float = float(input("Introduce un numero flotante o real: "))
+num_complex = complex(input("Introduce un numero complejo: "))
+
+print("String: ",palabra)
+print("Numero entero: ",num_int)
+print("Numero flotante: ",num_float)
+print("Numero complejo: ",num_complex)
